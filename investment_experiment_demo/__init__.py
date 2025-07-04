fd4495b0f5412f67a345bf24cd205b4fc835e5f9
unknown <alonw97@gmail.com>
2025-07-04 16:10:40 +0300

    better pages  naming

--- investment_experiment_demo/__init__.py
+++ investment_experiment_demo/__init__.py
@@ -361,7 +361,7 @@ class Disqualified(Page):
         return {'message': 'You have been disqualified from the experiment due to too many incorrect answers.'}
 
 page_sequence = [
-    ClientSettingsPage,
+    #ClientSettingsPage,
     Instructions,
     AuthenticationQuestion,
     BeforePartA,
